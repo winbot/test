cdda35990a829bc229892b22811221aa8ea48ba7
winbot <winbot.vl@gmail.com>
2016-04-03 20:36:53 +0300

    change css and detail_html_menu.html.twig

--- src/First/PageBundle/Controller/MainMenuController.php
+++ src/First/PageBundle/Controller/MainMenuController.php
@@ -3,6 +3,7 @@
  namespace First\PageBundle\Controller;
  use Symfony\Component\HttpFoundation\Response;
  use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+ use First\PageBundle\Entity\main_menu;
  
 class MainMenuController extends Controller
 {
@@ -15,19 +16,26 @@ class MainMenuController extends Controller
 		    $res_menu = $this->getDoctrine()->getRepository('FirstPageBundle:main_menu')->findAll();
 			if (!$res_menu)
 			{
-				throw $this->createNotFoundException('No menu found ');
+				throw $this->createNotFoundException('Not found menu');
 			}
 			$repo = 'FirstPageBundle:';
 			$repo .= $name_tab;
 			$res_tabl = $this->getDoctrine()->getRepository($repo)->findAll();
 			if (!$res_tabl)
 			{
-				throw $this->createNotFoundException('No hot_dishes_fish found ');
+				throw $this->createNotFoundException('Not found table Repository');
 			}
-			$col_menu=count($res_menu);
-			$col_tabl=count($res_tabl);
+			$col_menu = count($res_menu);
+			
+			$menu_name = $this->getDoctrine()->getRepository('FirstPageBundle:main_menu')->findOneBy(array ('nameTab' => $name_tab));
+			if (!$menu_name)
+			{
+				throw $this->createNotFoundException('No name menu found fot '.$name_tab);
+			}
+			
+			$col_tabl = count($res_tabl);
 			return $this->render('FirstPageBundle:FirstPage:detail_menu.html.twig', array('res_menu' => $res_menu, 'col_menu' => $col_menu,
-			'res_tabl' => $res_tabl, 'col_tabl' => $col_tabl));
+			'res_tabl' => $res_tabl, 'col_tabl' => $col_tabl, 'menu_name' => $menu_name));
 		}
 		elseif($name_tab=="2")
 		{
