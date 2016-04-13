4186094377cde24af6f99ea15e253bcdca364aed
winbot <winbot.vl@gmail.com>
2016-04-13 17:03:21 +0300

    add send detail order from page detail_menu.html.twig

--- src/First/PageBundle/Controller/SecurityPageController.php
+++ src/First/PageBundle/Controller/SecurityPageController.php
@@ -36,56 +36,8 @@ class SecurityPageController extends Controller
         return new Response('<html><body>Admin page!</body></html>');
     }
 
-    public function userAction($name_tab)
-    {
-        if($name_tab == "hot_dishes_fish" || $name_tab == "cold_dishes_fish" || $name_tab == "dessert" || $name_tab == "cold_dishes_meat" ||
-            $name_tab == "hot_snacks" || $name_tab == "main_dishes_meat" || $name_tab == "pasta" || $name_tab == "pizza" ||
-            $name_tab == "salads" || $name_tab == "soup")
-        {
-            //Формируем массив с названиями меню
-            //************************************
-            $res_menu = $this->getDoctrine()->getRepository('FirstPageBundle:main_menu')->findAll();
-            if (!$res_menu)
-            {
-                throw $this->createNotFoundException('Not found menu');
-            }
-            $col_menu = count($res_menu);
-            //************************************
-
-            //Формируем массив с детальным описанием меню
-            //************************************
-            $repo = 'FirstPageBundle:';
-            $repo .= $name_tab;
-            $res_tabl = $this->getDoctrine()->getRepository($repo)->findAll();
-            if (!$res_tabl)
-            {
-                throw $this->createNotFoundException('Not found table Repository');
-            }
-            $col_tabl = count($res_tabl);
-            //************************************
-
-            //Получаем мвссив элементов в котором содержится имя текущего меню для вывода на страницу
-            //************************************
-            $menu_name = $this->getDoctrine()->getRepository('FirstPageBundle:main_menu')->findOneBy(array ('nameTab' => $name_tab));
-            if (!$menu_name)
-            {
-                throw $this->createNotFoundException('No name menu found for '.$name_tab);
-            }
-            //************************************
-
-            //Вызываем страницу
-            //****************
-            return $this->render('FirstPageBundle:FirstPage:zakaz_menu.html.twig', array('res_menu' => $res_menu, 'col_menu' => $col_menu,
-                'res_tabl' => $res_tabl, 'col_tabl' => $col_tabl, 'menu_name' => $menu_name));
-            //****************
-        }
-        else
-        {
-            return new Response('<html><body>Detail menu not found!</body></html>');
-        }
-        //return new Response('<html><body>User page!</body></html>');
-    }
-
+    //Перенаправляем пользователя после регистрации 
+    //eсли ADMIN то на админ. страницу, иначе на страницу оформления заказа
     public function accountAction()
     {
         //$user = $this->container->get('security.context')->getToken()->getUser();
