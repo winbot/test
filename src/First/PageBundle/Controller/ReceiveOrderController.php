a077c3be8ade0cdb47c106e58cd8ee0c18854a9e
winbot <winbot.vl@gmail.com>
2016-04-16 16:02:03 +0300

    change procedure order receive

--- src/First/PageBundle/Controller/ReceiveOrderController.php
+++ src/First/PageBundle/Controller/ReceiveOrderController.php
@@ -9,73 +9,89 @@ class ReceiveOrderController extends Controller
 {
     //Обрабатываем данные отправленые в корзину
 	public function receiveAction(Request $request)
-    {
-		//Получаем значения данных из запроса
-		//Заполняем массив значениями id таблицы 'nametab'
-		$arr = array();
-		$col = $request->request->get('col');
-		for($i = 0; $i<=$col; $i++){
-			$id = $request->request->get('id'.$i);
-			if($id != null)$arr[] = $id;
-		}
-
-		$nametab = $request->request->get('nametab');
-		//создаём временную таблицу с именем пользователя
+	{
+		//создаём временную таблицу с именем пользователя в названии
 		$user = $this->getUser();
-		$user_name = '';
-        $state = null;//результат операции создания таблицы
-		if($user) {
-            $user_name = $user->getUsername(); //Получаем имя текущего пользователя
-            $user_name = str_replace(" ","",$user_name);
-            $em = $this->getDoctrine()->getEntityManager();
-            $connection = $em->getConnection();
-            $query = 'CREATE TABLE IF NOT EXISTS ' . $user_name . ' (
-  id INT(11) NOT NULL AUTO_INCREMENT,
-  name VARCHAR(255) COLLATE utf8_unicode_ci NOT NULL,
-  portion VARCHAR(255) COLLATE utf8_unicode_ci NOT NULL,
-  cost DECIMAL(10,2) NOT NULL,
-  composition LONGTEXT COLLATE utf8_unicode_ci NOT NULL,
-  PRIMARY KEY (id)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=5 ;';
-            $statement = $connection->prepare($query);
-            //$statement = $connection->prepare('CREATE TABLE t (c CHAR(20) CHARACTER SET utf8 COLLATE utf8_bin);');
-            $state = $statement->execute();
-        }
-
-		return $this->render('FirstPageBundle:FirstPage:test.html.twig', array(
-			'nametab' => $nametab,
-			'id' => $id[1],
-			'col' => $col,
-			'request' => $request,
-			'arr' => $arr,
-			'state' => $state,
-		));
-/*		$result = $this->getDoctrine()->getRepository('FirstPageBundle:main_menu')->findAll();
-			if (!$result)
-			{
-				throw $this->createNotFoundException('No menu found ');
+		$state1 = null;//результат операции создания таблицы
+		$state2 = null;//результат операции записи в таблицу
+		if ($user) {
+			$table_name = $user->getUsername(); //Получаем имя текущего пользователя
+			//$user_name = str_replace(" ", "", $user_name);// настроено фильтром на странице
+			$em = $this->getDoctrine()->getEntityManager();
+			$connection = $em->getConnection();
+			$query = 'CREATE TABLE IF NOT EXISTS ' . $table_name . ' (
+			  id INT(11) NOT NULL AUTO_INCREMENT,
+			  date_t DATETIME NOT NULL,
+			  portion VARCHAR(255) COLLATE utf8_unicode_ci NOT NULL,
+			  id_dishes INT(11) NOT NULL,
+			  name_dishes VARCHAR(255) COLLATE utf8_unicode_ci NOT NULL,
+			  cost DECIMAL(10,2) NOT NULL,
+			  name_tab VARCHAR(255) COLLATE utf8_unicode_ci NOT NULL,
+			  user_name VARCHAR(255) COLLATE utf8_unicode_ci NOT NULL,
+			  PRIMARY KEY (id)
+			) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=5 ;';
+			$statement = $connection->prepare($query);
+			//$statement = $connection->prepare('CREATE TABLE t (c CHAR(20) CHARACTER SET utf8 COLLATE utf8_bin);');
+			$state1 = $statement->execute();
+			if ($state1) {
+				//Заносим в таблицу данные о выбраных позициях
+				//Формируем дату и время
+				$date_time = date("Y-m-d H:i:s");
+				//Имя текущего пользователя
+				$user_name = $table_name;
+				//Получаем значения данных из запроса
+				//Название таблицы из основного меню
+				$name_tab = $request->request->get('nametab');
+				//Количество блюд в текущем меню
+				$col = $request->request->get('col');
+				//В цикле записываем в таблицу все позиции выбранные в текущем меню
+				$element = null;
+				for ($i = 0; $i <= $col; $i++) {
+					$id_dishes = $request->request->get('id' . $i);
+					$name_dishes = $request->request->get('name' . $i);
+					$portion = $request->request->get('portion' . $i);
+					$cost = floatval($request->request->get('cost' . $i));
+					if ($id_dishes != null) {
+						$query = 'INSERT INTO user1(date_t, portion, id_dishes, name_dishes, cost, name_tab, user_name) VALUES (?,?,?,?,?,?,?);';
+						$statement = $connection->prepare($query);
+						$statement->bindValue(1, $date_time);
+						$statement->bindValue(2, $portion);
+						$statement->bindValue(3, $id_dishes);
+						$statement->bindValue(4, $name_dishes);
+						$statement->bindValue(5, $cost);
+						$statement->bindValue(6, $name_tab);
+						$statement->bindValue(7, $user_name);
+						$state2 = $statement->execute();
+					}
+				}
 			}
-			$col=count($result);
-		    return $this->render('FirstPageBundle:FirstPage:index.html.twig', array(
-				'result' => $result,
-				'col' => $col,
-			));*/
+		}
+		
+		//Возвращаемся на страницу для продолжения заказа
+		return $this->redirect($this->generateUrl('main_menu', array('name_tab' => $name_tab)));
 	}
+
+
 	//Подтверждаем заказ
 	public function confirmAction(Request $request)
 	{
+		//Получаем из запроса данные для обработки
 		$nametab = $request->request->get('nametab');
-		//создаём временную таблицу с именем пользователя
+
+		//Переносим данные о заказе из временной в общую таблицу заказов
 		$user = $this->getUser();
 		$user_name = '';
 		$state = null;//результат операции создания таблицы
 		if($user) {
+
+
+
 			$user_name = $user->getUsername(); //Получаем имя текущего пользователя
 			$em = $this->getDoctrine()->getEntityManager();
 			$connection = $em->getConnection();
-			$query = 'DROP TABLE ' . $user_name;
+			$query = 'DROP TABLE IF EXISTS ' . $user_name;
 			$statement = $connection->prepare($query);
-			$state = $statement->execute();
+			$state = $statement->execute();//Удаляем временную таблицу
 		}
 	}
 }
